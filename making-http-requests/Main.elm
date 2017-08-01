21007968456956e3e6e17f5d440881fe98c8ce90
Neeraj Singh <neeraj@bigbinary.com>
2017-08-01 19:25:25 -0400

    wip

--- making-http-requests/Main.elm
+++ making-http-requests/Main.elm
@@ -3,6 +3,7 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Http
 
 
 main =
@@ -18,8 +19,23 @@ model =
     "Please wait ...."
 
 
+planetInfo : Cmd Msg
+planetInfo =
+    let
+        url =
+            "swapi.co/api/planets/1/"
+
+        request =
+            Http.getString url
+
+        cmd =
+            Http.send PlanetInMotion request
+    in
+    cmd
+
+
 type Msg
-    = Noop
+    = PlanetInMotion (Result Http.Error String)
 
 
 update : Msg -> Model -> Model
