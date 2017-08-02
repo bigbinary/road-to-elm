99da243b4cb502a967f3878d668ea7b63cb7ad18
Neeraj Singh <neeraj@bigbinary.com>
2017-08-01 21:06:58 -0400

    before and after change

---
+++ making-http-requests/BeforeChange.elm
@@ -0,0 +1,32 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+
+main =
+    Html.beginnerProgram { model = model, view = view, update = update }
+
+
+type alias Model =
+    String
+
+
+model : Model
+model =
+    "Finding planet ...."
+
+
+type Msg
+    = Noop
+
+
+update : Msg -> Model -> Model
+update msg model =
+    model
+
+
+view : Model -> Html Msg
+view model =
+    div [] [ text (toString model) ]
